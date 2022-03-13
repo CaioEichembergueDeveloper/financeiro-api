43ea6fc615003dc1e6a4b3f8d23af97f67c2add1
CaioEichembergueDeveloper <caio.allexandre95@hotmail.com>
2022-03-12 21:23:57 -0300

    Relacionamento de classes

--- src/main/java/com/caio/financeiro/domain/Despesa.java
+++ src/main/java/com/caio/financeiro/domain/Despesa.java
@@ -1,16 +1,34 @@
 package com.caio.financeiro.domain;
 
+import java.io.Serializable;
 import java.util.Date;
 import java.util.Objects;
 
-public class Despesa {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
+@Entity
+public class Despesa implements Serializable{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String descricao;
 	private Date primeiroPagamento;
 	private Double precoUnico;
 	private Integer qtdeParcelas;
-
+	
+	@ManyToOne
+	@JoinColumn(name = "categoria_id")
 	private Categoria categoria;
 
 	public Despesa() {
